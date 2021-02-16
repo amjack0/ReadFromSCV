49f0a7bc5f0410c87cfc9d287f982f7787a24783
mujuburrehman.gouri <mujuburrehman.gouri@hansrobot.de>
2021-02-16 19:41:53 +0100

    Read and Plot Till Maximum Index

--- main.cpp
+++ main.cpp
@@ -12,20 +12,18 @@ namespace plt = matplotlibcpp;
 
 class ReadCsv{
 
-protected:
-  std::vector<double> plotPoints;
-
 public:
 
   ReadCsv()
   {
-    cout << "[CSV] Constructed !" << endl;
   }
   ~ReadCsv(){}
 
-  std::vector<std::pair<std::string, std::vector<double>>> readFile (std::string filename, unsigned int MaxIndex){
-    /** Reads a CSV file into a vector of <string, vector<int>> pairs where
+  std::vector<std::pair<std::string, std::vector<double>>> readFile (std::string filename, int MaxIndex){
+    /** Reads a CSV file into a vector of <string, vector<double>> pairs where
      each pair represents <column name, column values>*/
+
+    if(MaxIndex <= 0.0) throw std::runtime_error("[CSV] Nr of Colums should be more than Zero !");
 
     // Create a vector of <string, double vector> pairs to store the result
     std::vector<std::pair<std::string, std::vector<double>>> result;
@@ -36,47 +34,45 @@ public:
     if(!myFile.is_open()) throw std::runtime_error("[CSV] Could not open file");
     std::string line, colname;
     double val;
-
+    int count =0;
     if(myFile.good())
     {
-      cout << "[CSV] Inside !" << endl;
       // Extract the first line in the file
       std::getline(myFile, line);
 
       // Create a stringstream from line
       std::stringstream ss(line);
       // Extract each column name
-
-      while(std::getline(ss, colname, ',')){
-
+      std::cout << "[CSV] Reading " << MaxIndex << " columns." << std::endl;
+      while(std::getline(ss, colname, ',') && count<MaxIndex){
+        count ++;
         // Initialize and add <colname, double vector> pairs to result
         result.push_back({colname, std::vector<double> {}});
       }
-    }
 
-    // Read data, line by line
-    while(std::getline(myFile, line))
-    {
-      // Create a stringstream of the current line
-      std::stringstream ss(line);
+      // Read data, line by line
+      while(std::getline(myFile, line)  )
+      {
+        // Create a stringstream of the current line
+        std::stringstream ss(line);
+        count=0;
+        // Keep track of the current column index
+        unsigned int colIdx = 0;
 
-      // Keep track of the current column index
-      unsigned int colIdx = 0;
+        // Extract each double value till Max colum index
+        while(ss >> val && count<MaxIndex ){
 
-      // Extract each double value till colum index is 26
-      while(ss >> val){
+          // Add the current integer to the 'colIdx' column's values vector
+          result.at(colIdx).second.push_back(val);
+          count++;
+          // If the next token is a comma, ignore it and move on
+          if(ss.peek() == ',') ss.ignore();
 
-        // Add the current integer to the 'colIdx' column's values vector
-        result.at(colIdx).second.push_back(val);
-
-        // If the next token is a comma, ignore it and move on
-        if(ss.peek() == ',') ss.ignore();
-
-        // Increment the column index
-        colIdx++;
+          // Increment the column index
+          colIdx++;
+        }
       }
     }
-
     myFile.close();
     return result;
   }
@@ -85,7 +81,7 @@ public:
 
     for(const auto& it : output)
      {
-         string colName = it.first; cout << "###### column Name: " << colName << endl;
+         string colName = it.first; cout << "[CSV] column Name: " << colName << endl;
          std::vector<double> dataVect = it.second;
          std::vector<double> points;
 
@@ -95,7 +91,7 @@ public:
            points.push_back(entry);
          }
          plt::named_plot(colName, points, "r"); plt::title("Trajectory");
-         plt::legend(); plt::show();
+         plt::legend(); plt::show(); //plt::save("/home/mujib/Desktop/Samplefile.pgf");
      }
   }
 
@@ -106,7 +102,7 @@ public:
 int main()
 {
   ReadCsv Obj;
-  std::vector<std::pair<std::string, std::vector<double>>> output = Obj.readFile("/home/mujib/Desktop/experiment1.csv", 26);
+  std::vector<std::pair<std::string, std::vector<double>>> output = Obj.readFile("/home/mujib/Desktop/experiment1.csv", 2);
   Obj.plotData(output);
 
   return 0;
